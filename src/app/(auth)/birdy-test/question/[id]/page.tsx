4742ddbf839fabb5b94dbb6c4cc9b1855e9e2ce1
hhshhhskk <hhshhhskk@naver.com>
2025-03-25 11:10:28 +0000

    hotfix: 배포에러2

--- src/app/(auth)/birdy-test/question/[id]/page.tsx
+++ src/app/(auth)/birdy-test/question/[id]/page.tsx
@@ -1,15 +1,18 @@
 import BirdyTestForm from "@/components/signup/birdyTestTemp/BirdyTestForm";
 import { notFound } from "next/navigation";
 
-export default function BirdyTestQuestion({
-  params,
-}: {
-  params: { id: string };
-}) {
+// PageProps 타입 정의
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function BirdyTestQuestion({ params }: PageProps) {
   const step = Number(params.id);
 
   if (isNaN(step) || step < 1 || step > 12) {
-    return notFound();
+    return notFound(); // 범위 밖의 값이면 404 페이지 처리
   }
 
   return <BirdyTestForm step={step} />;
