4799751ce3648d1555940eb683a72714df2e3f2c
hhshhhskk <hhshhhskk@naver.com>
2025-03-25 11:28:23 +0000

    hotfix: 배포에러4

--- src/app/(auth)/birdy-test/question/[id]/page.tsx
+++ src/app/(auth)/birdy-test/question/[id]/page.tsx
@@ -1,16 +1,12 @@
+"use client"; // 클라이언트 컴포넌트로 지정
+
+import { useParams } from "next/navigation";
 import BirdyTestForm from "@/components/signup/birdyTestTemp/BirdyTestForm";
 import { notFound } from "next/navigation";
 
-// PageProps 타입 정의
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
-// 비동기적으로 처리하는 방식 적용
-export default async function BirdyTestQuestion({ params }: PageProps) {
-  const step = Number(params.id);
+export default function BirdyTestQuestion() {
+  const { id } = useParams();
+  const step = Number(id);
 
   if (isNaN(step) || step < 1 || step > 12) {
     return notFound(); // 범위 밖의 값이면 404 페이지 처리
