3a39bc7d996443d9a962ea5149e95d4fead1440a
hhshhhskk <hhshhhskk@naver.com>
2025-03-31 13:34:15 +0000

    feat: 마이페이지 닉네임 수정 준비중표시

--- src/app/(footershare)/mybirdy/(no-footer)/my-account/page.tsx
+++ src/app/(footershare)/mybirdy/(no-footer)/my-account/page.tsx
@@ -5,48 +5,77 @@ import Image from "next/image";
 
 const Page = () => {
   return (
-    <>
-      <CommonHeader title="내 정보 수정" addPaddingX />
+    // <>
+    //   <CommonHeader title="내 정보 수정" addPaddingX />
 
-      <div className="w-full bg-line01 px-global py-[10px]">
-        <h2 className="text-Body1_M_16 text-gray06">닉네임</h2>
+    //   <div className="w-full bg-line01 px-global py-[10px]">
+    //     <h2 className="text-Body1_M_16 text-gray06">닉네임</h2>
+    //   </div>
+
+    //   <div className="px-global">
+    //     <div className="my-[14px] w-full bg-line02 border-1 border-gray01 rounded-[10px] px-5 py-global text-gray06">
+    //       <p className="text-Caption1_R_12">현재 설정된 닉네임</p>
+
+    //       <div className="flex items-center justify-between">
+    //         <p className="text-Body1_B_18">사용자 닉네임</p>
+
+    //         <button className="flex items-center pl-1 pr-2 py-1 gap-0.5 rounded-[6px] bg-black01">
+    //           <Image
+    //             src="/images/common/icon-edit.svg"
+    //             alt="수정 아이콘"
+    //             width={24}
+    //             height={24}
+    //           />
+    //           <span className="text-gray01 text-Caption1_B_12">수정</span>
+    //         </button>
+    //       </div>
+    //     </div>
+
+    //     <ul className="ml-6 list-disc list-outside text-gray04 text-Body2_R_14">
+    //       <li>
+    //         <span>닉네임을 변경할 경우 14일 이후에 다시 변경할 수 있어요.</span>
+    //       </li>
+    //       <li>
+    //         <span>특수문자는 사용할 수 없어요. (예: @, #, $, %, &, ~)</span>
+    //       </li>
+    //     </ul>
+    //   </div>
+
+    //   {/* <div className="w-full px-4 ">
+    //     <button className="w-full px-4 py-[13px] bg-[#292D32] text-white text-[16px] leading-[24px] font-medium rounded-[12px] flex items-center justify-center select-none cursor-pointer">
+    //       수정완료
+    //     </button>
+    //   </div> */}
+    // </>
+    <div className="flex flex-col items-center px-global">
+      <CommonHeader title="닉네임 수정" />
+
+      <Image
+        src={`/images/my-birdy/birdy_collection.png`}
+        alt="닉네임 수정"
+        width={198}
+        height={152}
+        className="mt-6"
+      />
+      <p className="text-[#8E8E93] text-center text-base font-bold leading-[24px] tracking-[-0.064px] mt-5">
+        곧 업데이트 예정이에요
+      </p>
+      <p className="text-[#8E8E93] text-center text-base font-normal leading-[24px] tracking-[-0.064px] mt-4">
+        닉네임을 바꾸고 싶다면
+        <br />
+        디어버디 고객센터에 문의해주세요.
+        <br />
+        빠르고 친절하게 해결해드릴게요.
+      </p>
+      <div className="flex w-full p-[16px_20px] flex-col items-start rounded-[10px] border border-[#E5E5EA] bg-[#F0F1EC] mt-5">
+        <p className="text-[#6B7178] text-xs font-normal leading-[16px] tracking-[-0.048px]">
+          디어버디 고객센터
+        </p>
+        <p className="text-[#6B7178] text-sm font-medium leading-[20px] tracking-[-0.056px] mt-1.5">
+          dearbirdy@dearbirdy.xyz
+        </p>
       </div>
-
-      <div className="px-global">
-        <div className="my-[14px] w-full bg-line02 border-1 border-gray01 rounded-[10px] px-5 py-global text-gray06">
-          <p className="text-Caption1_R_12">현재 설정된 닉네임</p>
-
-          <div className="flex justify-between items-center">
-            <p className="text-Body1_B_18">사용자 닉네임</p>
-
-            <button className="flex items-center pl-1 pr-2 py-1 gap-0.5 rounded-[6px] bg-black01">
-              <Image
-                src="/images/common/icon-edit.svg"
-                alt="수정 아이콘"
-                width={24}
-                height={24}
-              />
-              <span className="text-gray01 text-Caption1_B_12">수정</span>
-            </button>
-          </div>
-        </div>
-
-        <ul className="list-outside list-disc ml-6 text-gray04 text-Body2_R_14">
-          <li>
-            <span>닉네임을 변경할 경우 14일 이후에 다시 변경할 수 있어요.</span>
-          </li>
-          <li>
-            <span>특수문자는 사용할 수 없어요. (예: @, #, $, %, &, ~)</span>
-          </li>
-        </ul>
-      </div>
-
-      {/* <div className="w-full px-4 ">
-        <button className="w-full px-4 py-[13px] bg-[#292D32] text-white text-[16px] leading-[24px] font-medium rounded-[12px] flex items-center justify-center select-none cursor-pointer">
-          수정완료
-        </button>
-      </div> */}
-    </>
+    </div>
   );
 };
 
