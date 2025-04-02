e811e3f86aa198f88878f034d80483b9e252bc75
hhshhhskk <hhshhhskk@naver.com>
2025-04-02 12:27:05 +0000

    feat: 홈화면 이미지 로티로 변경

--- src/components/home/HomeMainSection.tsx
+++ src/components/home/HomeMainSection.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import HomeLetterIcon from "@/components/Icons/Home_letter_icon";
-import Image from "next/image";
 import SendMessage from "./SendMessage";
 import SendMessageLimit from "./SendMessageLimit";
 import { birdNameMap } from "@/constants/birdNameMap";
 import { IUserData } from "@/app/(footershare)/home/page";
 import StyledButton from "../ui/StyledButton";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false }); // 서버 사이드 렌더링 방지
 
 interface HomeMainSectionProps {
   userData?: IUserData;
@@ -23,9 +25,7 @@ export default function HomeMainSection({
 }: HomeMainSectionProps) {
   const router = useRouter();
 
-  const birdKey = userData?.birdName
-    ? birdNameMap[userData.birdName]
-    : "default";
+  const [animationData, setAnimationData] = useState(null);
 
   const buttonText = userRole === "MENTOR" ? "편지 보기" : "편지 쓰기";
   const buttonRoute = userRole === "MENTOR" ? "/letters" : "/send";
@@ -33,6 +33,17 @@ export default function HomeMainSection({
     userRole === "MENTOR"
       ? "고민을 들어주러 오셨군요!"
       : "오늘은 무슨 고민이 있나요?";
+
+  /** ✅ 애니메이션 로드 */
+  useEffect(() => {
+    const myBirdName = userData?.birdName;
+    if (!myBirdName) return; // ✅ myBirdName이 없으면 실행하지 않음
+
+    const birdKey = birdNameMap[myBirdName] || "parrot";
+    import(`@/animations/${birdKey}_main.json`)
+      .then((data) => setAnimationData(data.default))
+      .catch((err) => console.error("❌ 애니메이션 로드 실패:", err));
+  }, [userData?.birdName]); // ✅ myBirdName이 설정된 후 실행
 
   return (
     <div
@@ -44,13 +55,16 @@ export default function HomeMainSection({
         <SendMessage userData={userData as IUserData} />
       )}
 
-      <Image
+      {/* <Image
         src={`/images/birds/home/${birdKey}.png`}
         alt="홈 나의 새 프로필사진"
         width={280}
         height={226}
-        className="mt-global mb-6"
-      />
+        className="mb-6 mt-global"
+      /> */}
+      <div>
+        <Lottie animationData={animationData} loop={true} autoplay={true} />
+      </div>
 
       <div className="text-center mb-global">
         <p className="text-Body1_B_16 mb-[2px]">
